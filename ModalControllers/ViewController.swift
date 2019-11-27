20c6667a4dcce50587db6bcf1a525de97a21d5a7
Daniil <rodindaniil@gmail.com>
2019-11-27 15:27:29 +0400

    Initial Commit

---
+++ ModalControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModalControllers
+//
+//  Created by Даниил on 27.11.2019.
+//  Copyright © 2019 Даниил. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
